84d2b614c7dbd19d7b966b8a319c134f89c41490
jimaichiro <54446706+YuichiroIijima@users.noreply.github.com>
2019-09-25 18:18:29 +0900

    css separation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-var app = require("express")();
+var express = require("express");
+var app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.use(express.static(__dirname));
 
 io.on("connection", function(socket) {
   socket.on("chat message", function(msg) {
